4e845bf13a5dd9d82f6147500c4b893c33a46440
VishnuThobbi <59354450+VishnuThobbi@users.noreply.github.com>
2020-10-09 14:35:11 +0530

    Add files via upload

--- src/com/hsbc/controller/CitizenController.java
+++ src/com/hsbc/controller/CitizenController.java
@@ -21,15 +21,18 @@ public class CitizenController extends HttpServlet{
 		String[] header = str.split("/");
 		int length=header.length-1;
 		System.out.println(header[length]);
-		if(header[length].equals("Login.html")) {
-			RequestDispatcher rd = req.getRequestDispatcher("First.jsp"); //Redirect to another page
+		if(header[length].equals("CitizenLogin.html")) {
+			RequestDispatcher rd = req.getRequestDispatcher("First.jsp"); //Redirect to page after login
 			rd.forward(req, resp);
 		}
-		else {
-			RequestDispatcher rd = req.getRequestDispatcher("First2.jsp");//Redirect where required
+		else if(header[length].equals("CitizenRegistration.html")){
+			RequestDispatcher rd = req.getRequestDispatcher("First2.jsp");//Redirect to page after citizen registration
 			rd.forward(req, resp);
 		}
-		
+		else if(header[length].equals("ApplyScheme.html")){
+			RequestDispatcher rd = req.getRequestDispatcher("Welcome.jsp");//Redirect to page after apply scheme
+			rd.forward(req, resp);
+		}
 		/*
 		 * int header = Integer.parseInt(req.getParameter("prodId"));
 		 * System.out.println(header);
